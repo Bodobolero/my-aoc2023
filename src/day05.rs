c587a366cb14fe6a1ccc70a4eedd1c57b10cfae8
Peter Bendel <peter@familie-bendel.de>
2023-12-05 12:29:32 +0100

    day5 solution part1, part2 too slow

--- src/day05.rs
+++ src/day05.rs
@@ -1,97 +1,127 @@
 #![feature(test)]
 
+use rustc_hash::FxHashMap;
+use std::collections::HashMap;
+
 extern crate test;
 
 const INPUT: &str = include_str!("../inputs/input05.txt");
 
-use regex::Regex;
-
-lazy_static::lazy_static! {
-    static ref RE_MOVES: Regex = Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap();
+#[derive(Debug)]
+pub struct Map<'a> {
+    to: &'a str,
+    // a range is a tuple (destination range start, source range start, range length)
+    ranges: Vec<(usize, usize, usize)>,
+    history: FxHashMap<usize, usize>,
 }
 
-fn part1() -> String {
-    let mut parts = INPUT.split("\n\n");
-    let mut topology = parts.next().unwrap().lines().rev();
-    let moves = parts.next().unwrap().lines();
-    let stack_count = (topology.next().unwrap().len() + 2) / 4;
-    println!("We have {} stacks", stack_count);
-    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); stack_count];
-    for line in topology {
-        for i in 0..stack_count {
-            let char = line.chars().nth(i * 4 + 1).unwrap();
-            if char != ' ' {
-                stacks[i].push(char);
+// a map with "from" as key and the map as value
+type MapMap<'a> = HashMap<&'a str, Map<'a>>;
+
+// map seed to location
+fn map(seed: usize, mapmap: &mut MapMap) -> usize {
+    let mut from = "seed";
+    let mut value = seed;
+
+    'outer: loop {
+        let map = mapmap.get_mut(from).unwrap();
+        if let Some(mapped) = map.history.get(&value) {
+            print!("+");
+            value = *mapped;
+            break 'outer;
+        }
+        let mut new_value = value;
+        'inner: for (dest_range_start, src_range_start, range_len) in &map.ranges {
+            if value >= *src_range_start && value < *src_range_start + range_len {
+                new_value = *dest_range_start + (value - *src_range_start);
+                break 'inner;
             }
         }
-    }
-    println!("stacks: {:?}", stacks);
-
-    for line in moves {
-        let caps = RE_MOVES.captures(line).unwrap();
-        let (count, from, to) = (
-            caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-            caps.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1,
-            caps.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1,
-        );
-        for _i in 0..count {
-            let char = stacks[from].pop().unwrap();
-            stacks[to].push(char);
+        map.history.insert(value, new_value);
+        print!("-");
+        value = new_value;
+        if map.to == "location" {
+            break 'outer;
         }
+        from = map.to;
     }
-
-    let mut result: String = "".to_string();
-    for mut stack in stacks {
-        result.push(stack.pop().unwrap());
-    }
-    result
+    value
 }
 
-fn part2() -> String {
-    let mut parts = INPUT.split("\n\n");
-    let mut topology = parts.next().unwrap().lines().rev();
-    let moves = parts.next().unwrap().lines();
-    let stack_count = (topology.next().unwrap().len() + 2) / 4;
-    println!("We have {} stacks", stack_count);
-    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); stack_count];
-    for line in topology {
-        for i in 0..stack_count {
-            let char = line.chars().nth(i * 4 + 1).unwrap();
-            if char != ' ' {
-                stacks[i].push(char);
-            }
-        }
+fn parse_input<'a>(input: &'a str) -> (Vec<usize>, MapMap) {
+    let mut mapmap: MapMap<'a> = HashMap::new();
+    let mut sections = input.split("\n\n");
+    // parse seeds
+    let seeds_str = sections.next().unwrap();
+    let mut seeds_split = seeds_str.split(": ");
+    seeds_split.next();
+    let seeds: Vec<usize> = seeds_split
+        .next()
+        .unwrap()
+        .split(' ')
+        .map(|nr_as_str| nr_as_str.parse::<usize>().unwrap())
+        .collect();
+    for section in sections {
+        let mut lines = section.lines();
+        let mut name_split = lines.next().unwrap().split(' ');
+        name_split = name_split.next().unwrap().split('-');
+        let from = name_split.next().unwrap();
+        name_split.next();
+        let to = name_split.next().unwrap();
+        // println!("map from {} to {}", from, to);
+        let ranges: Vec<(usize, usize, usize)> = lines
+            .map(|s| {
+                let mut nrs = s.split(' ').map(|nr_str| nr_str.parse::<usize>().unwrap());
+                let dest_range_start = nrs.next().unwrap();
+                let src_range_start = nrs.next().unwrap();
+                let range_len = nrs.next().unwrap();
+                (dest_range_start, src_range_start, range_len)
+            })
+            .collect();
+        let map = Map {
+            to,
+            ranges,
+            history: FxHashMap::default(),
+        };
+        // println!("{:?}", map);
+        mapmap.insert(from, map);
     }
-    println!("stacks: {:?}", stacks);
 
-    for line in moves {
-        let caps = RE_MOVES.captures(line).unwrap();
-        let (count, from, to) = (
-            caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-            caps.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1,
-            caps.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1,
+    (seeds, mapmap)
+}
+
+fn part1(input: &str) -> usize {
+    let (seeds, mut mapmap) = parse_input(input);
+    // println!("{:?}", seeds);
+    seeds
+        .into_iter()
+        .map(|seed| map(seed, &mut mapmap))
+        .min()
+        .unwrap()
+}
+
+fn part2(input: &str) -> usize {
+    let (seeds, mut mapmap) = parse_input(input);
+    let mut local_minima: Vec<usize> = Vec::new();
+    for i in 0..seeds.len() / 2 {
+        let local_minimum = (seeds[i * 2]..seeds[i * 2] + seeds[i * 2 + 1])
+            .map(|seed| map(seed, &mut mapmap))
+            .min()
+            .unwrap();
+        println!(
+            "Local minimum: {} for range {}..{}",
+            local_minimum,
+            seeds[i * 2],
+            seeds[i * 2] + seeds[i * 2 + 1]
         );
-        let mut stack: Vec<char> = Vec::new();
-        for _i in 0..count {
-            let char = stacks[from].pop().unwrap();
-            stack.push(char)
-        }
-        for _i in 0..count {
-            let char = stack.pop().unwrap();
-            stacks[to].push(char);
-        }
+        local_minima.push(local_minimum);
     }
-
-    let mut result: String = "".to_string();
-    for mut stack in stacks {
-        result.push(stack.pop().unwrap());
-    }
-    result
+    *local_minima.iter().min().unwrap()
 }
 
 pub fn main() {
-    println!("Part 1: Answer {}", part1());
-    println!("Part 2: Answer {} ", part2());
+    println!("Part 1: Answer {}", part1(INPUT));
+    println!("Part 2: Answer {} ", part2(INPUT));
 }
 
 #[cfg(test)]
@@ -99,21 +129,66 @@ mod tests {
     use super::*;
     use test::Bencher;
 
+    const EXAMPLE: &str = r#"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"#;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(EXAMPLE), 35);
+    }
+
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), "HNSNMTLHQ".to_string());
+        assert_eq!(part1(INPUT), 174137457);
     }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), 46);
+    }
+
     #[test]
     fn part2_test() {
-        assert_eq!(part2(), "RNLFDJMCT".to_string());
+        assert_eq!(part2(INPUT), 0);
     }
     #[bench]
     fn part1_bench(b: &mut Bencher) {
-        b.iter(part1);
+        b.iter(|| part1(INPUT));
     }
 
     #[bench]
     fn part2_bench(b: &mut Bencher) {
-        b.iter(part2);
+        b.iter(|| part2(INPUT));
     }
 }
